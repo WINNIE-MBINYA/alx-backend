3a40f7066cafbb3e52252dc6607804707fd1ce19
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-07-22 18:12:03 +0300

    Correct Pycodestyle errors

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,6 +5,7 @@ Simple helper function for pagination.
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Calculate the start and end index for the given page and page size.
