2ed0102c01ea6a38f5516d374bc8a77a7843e3ff
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-07-24 20:36:30 +0300

    Add file

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -16,7 +16,8 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache """
         if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS and key not in self.cache_data:
+            if (len(self.cache_data) >= self.MAX_ITEMS
+                    and key not in self.cache_data):
                 print("DISCARD:", self.last_key)
                 del self.cache_data[self.last_key]
             self.cache_data[key] = item
